87ab459258eddba39dc2c5125cf19a9c98927702
Mykyta Prokaiev <mykytaprokaiev@gmail.com>
2023-09-09 02:10:59 +0200

    Implemented enums

---
+++ cfdtool/utils/classes.py
@@ -0,0 +1,71 @@
+import jax.numpy as jnp
+from jax.lax import dynamic_update_slice
+from utils.schemas import Node
+
+
+class Shape:
+    def __init__(self):
+        self.grid = self.create_grid()
+
+    def area(self):
+        raise NotImplementedError(
+            "This method should be implemented by the subclasses."
+        )
+
+    def create_grid(self):
+        raise NotImplementedError(
+            "This method should be implemented by the subclasses."
+        )
+
+
+class Rectangle(Shape):
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        super().__init__()
+
+    def create_grid(self) -> jnp.ndarray:
+        grid = jnp.ones((self.height, self.width))
+        return grid.astype(jnp.int32)
+
+
+class Circle(Shape):
+    def __init__(self, radius: int):
+        self.radius = radius
+        super().__init__()
+
+    def create_grid(self) -> jnp.ndarray:
+        diameter = 2 * self.radius
+
+        grid_points = int(diameter)
+
+        # Create a meshgrid
+        x, y = jnp.meshgrid(
+            jnp.linspace(-self.radius, self.radius, grid_points),
+            jnp.linspace(-self.radius, self.radius, grid_points),
+        )
+
+        # Calculate the distance from the center (0,0)
+        distance_from_center = jnp.sqrt(x**2 + y**2)
+
+        # Create a boolean mask for the circle
+        circle_mask = distance_from_center <= self.radius
+        return circle_mask.astype(jnp.int32)
+
+
+class Mesh:
+    def __init__(self, width: int, height: int) -> None:
+        self.grid = jnp.zeros((height, width)).astype(jnp.int32)
+
+    def place(self, shape: Shape, center: Node):
+        start_x = center.x - shape.grid.shape[0] // 2
+        start_y = center.y - shape.grid.shape[1] // 2
+
+        self.grid = dynamic_update_slice(
+            self.grid,
+            shape.grid,
+            (jnp.int32(start_x), jnp.int32(start_y)),
+        )
+
+    def get_grid(self):
+        return self.grid
--- cfdtool/utils/schemas.py
+++ cfdtool/utils/schemas.py
@@ -1,83 +1,30 @@
-import jax.numpy as jnp
-from jax.lax import dynamic_update_slice
+from enum import Enum
+
 from pydantic import BaseModel
+
+import jax.numpy as jnp
+
+
+class Lattice(Enum):
+    VELOCITIES = jnp.array(
+        [[0, 1, 0, -1, 0, 1, -1, -1, 1], [0, 0, 1, 0, -1, 1, 1, -1, -1]]
+    )
+    INDICES = jnp.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    WEIGHTS = jnp.array(
+        [4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36]
+    )
+    OPOSITE_INDECES = jnp.array([0, 3, 4, 1, 2, 7, 8, 5, 6])
+
+
+class NodeVelocity(Enum):
+    RIGHT = jnp.array([1, 5, 8])
+    UP = jnp.array([2, 5, 6])
+    LEFT = jnp.array([3, 6, 7])
+    DOWN = jnp.array([4, 7, 8])
+    PURE_VERTICAL = jnp.array([0, 2, 4])
+    PURE_HORIZONTAL = jnp.array([0, 1, 3])
 
 
 class Node(BaseModel):
     x: int
     y: int
-
-
-class Shape:
-    def __init__(self):
-        self.grid = self.create_grid()
-
-    def area(self):
-        raise NotImplementedError(
-            "This method should be implemented by the subclasses."
-        )
-
-    def create_grid(self):
-        raise NotImplementedError(
-            "This method should be implemented by the subclasses."
-        )
-
-
-class Rectangle(Shape):
-    def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
-        super().__init__()
-
-    def create_grid(self) -> jnp.ndarray:
-        grid = jnp.ones((self.height, self.width))
-        return grid.astype(jnp.int32)
-
-
-class Circle(Shape):
-    def __init__(self, radius: int):
-        self.radius = radius
-        super().__init__()
-
-    def create_grid(self) -> jnp.ndarray:
-        diameter = 2 * self.radius
-
-        grid_points = int(diameter)
-
-        # Create a meshgrid
-        x, y = jnp.meshgrid(
-            jnp.linspace(-self.radius, self.radius, grid_points),
-            jnp.linspace(-self.radius, self.radius, grid_points),
-        )
-
-        # Calculate the distance from the center (0,0)
-        distance_from_center = jnp.sqrt(x**2 + y**2)
-
-        # Create a boolean mask for the circle
-        circle_mask = distance_from_center <= self.radius
-        return circle_mask.astype(jnp.int32)
-
-
-class Mesh:
-    def __init__(self, width: int, height: int) -> None:
-        self.grid = jnp.zeros((height, width)).astype(jnp.int32)
-
-    def place(self, shape: Shape, center: Node):
-        start_x = center.x - shape.grid.shape[0] // 2
-        start_y = center.y - shape.grid.shape[1] // 2
-
-        self.grid = dynamic_update_slice(
-            self.grid,
-            shape.grid,
-            (jnp.int32(start_x), jnp.int32(start_y)),
-        )
-
-    def get_grid(self):
-        return self.grid
-
-
-mesh = Mesh(20, 20)
-circle = Circle(5)
-circle.create_grid()
-mesh.place(circle, Node(x=10, y=10))
-print(mesh.get_grid())
