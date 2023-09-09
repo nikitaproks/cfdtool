c7105509d89cbbd17997f398782b00a69c8ffa9b
FaroukHai <60575260+FaroukHai@users.noreply.github.com>
2023-09-09 13:17:46 +0200

    working simulation

--- cfdtool/main.py
+++ cfdtool/main.py
@@ -1,7 +1,11 @@
 import jax
+from simulation.circle_simulation import simulation
+
+
 def main():
     print("Hello World!")
-    jax.config.update("jax_enable_x64", True) # Needs to be 
+    jax.config.update("jax_enable_x64", True)  # Needs to be
+    simulation()
 
 
 if __name__ == "__main__":
--- cfdtool/utils/functions.py
+++ cfdtool/utils/functions.py
@@ -1,133 +1,217 @@
 import jax
 import jax.numpy as jnp
+from tqdm import tqdm
+from utils.schemas import Lattice, NodeVelocity, SimulationOutput
 
-# TODO: pull from github
-# TODO: implement enums
-# TODO: finish algorithn
-# TODO: debug
 
-def euclidean_norm(to_be_normed: jnp.array[float], norm_axis = -1) -> jnp.array[float]:
-    return jnp.linalg.norm(
-        to_be_normed,
-        axis = norm_axis,
-        ord = 2
+def euclidean_norm(to_be_normed: jnp.array, norm_axis=-1) -> jnp.array:
+    return jnp.linalg.norm(to_be_normed, axis=norm_axis, ord=2)
+
+
+def get_density(discrete_velocities: jnp.array) -> jnp.array:
+    density = jnp.sum(discrete_velocities, axis=-1)
+    return jnp.array(density)
+
+
+def get_macroscopic_velocities(
+    discrete_velocities: jnp.array, density: jnp.array
+) -> jnp.array:
+    macroscopic_velocities = (
+        jnp.einsum(
+            "NMQ,dQ->NMd",
+            discrete_velocities,
+            Lattice.VELOCITIES,
+        )
+        / density[..., jnp.newaxis]
     )
-
-def get_density(discrete_velocities: jnp.array[float]) -> jnp.array[float]:
-
-    density = jnp.sum(discrete_velocities, axis = -1)
-    return density
-
-def get_macroscopic_velocities(discrete_velocities: jnp.array[float],
-                                density: jnp.array[float]) -> jnp.array[float]:
-    macroscopic_velocities = jnp.einsum(
-        "NMQ,dQ->NM",
-        discrete_velocities,
-        LATTICE_VELOCITES, # TODO: replace
-    ) / density[...,jnp.newaxis]
     return macroscopic_velocities
 
-def get_equilibrium_discrete_velocites(macroscopic_velocites: jnp.array[float],
-                                       density: jnp.array[float]) -> jnp.array[float]:
-    
+
+def get_equilibrium_discrete_velocities(
+    macroscopic_velocites: jnp.array, density: jnp.array
+) -> jnp.array:
     projected_discrete_velocites = jnp.einsum(
-        "dQ,NMd -> NMQ",
-        LATTICE_VELOCITES,
-        macroscopic_velocites
+        "dQ,NMd -> NMQ", Lattice.VELOCITIES, macroscopic_velocites
     )
-    macroscopic_velocites_magnitude = euclidean_norm(projected_discrete_velocites)
+    macroscopic_velocites_magnitude = euclidean_norm(
+        projected_discrete_velocites
+    )
     equilibrium_discrete_velocites = (
         density[..., jnp.newaxis]
-        *
-        LATTICE_WEIGHTS[jnp.newaxis, jnp.newaxis, :]
-        *
-        (
+        * Lattice.WEIGHTS[jnp.newaxis, jnp.newaxis, :]
+        * (
             1
-            +
-            3 * projected_discrete_velocites
-            + 
-            9/2 * projected_discrete_velocites ** 2
-            - 
-            3/2 * macroscopic_velocites_magnitude[..., jnp.newaxis]**2
+            + 3 * projected_discrete_velocites
+            + 9 / 2 * projected_discrete_velocites**2
+            - 3 / 2 * macroscopic_velocites_magnitude[..., jnp.newaxis] ** 2
         )
     )
     return equilibrium_discrete_velocites
 
-def calculate_kinematic_viscocity(velocity: float, 
-                                  length_scale: float, 
-                                  reynolds_number: float) -> float:
+
+def calculate_kinematic_viscocity(
+    velocity: float, length_scale: float, reynolds_number: float
+) -> float:
     """
     Implicit value defined by the Reynolds Number.
     """
-    return (velocity*length_scale)/reynolds_number
+    return (velocity * length_scale) / reynolds_number
+
 
 def relaxation_omega(kinematic_viscocity: float) -> float:
-    return 1.0/(3.0*kinematic_viscocity*0.5)
+    return 1.0 / (3.0 * kinematic_viscocity * 0.5)
 
+
+# TODO: unused, delete
 def generate_mesh(x_points: int, y_points: int) -> tuple[jnp.array, jnp.array]:
     x = jnp.arrange(x_points)
     y = jnp.arrange(y_points)
-    X, Y = jnp.meshgrid(x, y, 'ij')
+    X, Y = jnp.meshgrid(x, y, "ij")
     return (X, Y)
 
-def initialize_velocity(inlet_velocity: float, x_points: int, y_points: int) -> jnp.array:
+
+def initialize_velocity(
+    inlet_velocity: float, x_points: int, y_points: int
+) -> jnp.array:
     velocity_profile = jnp.zeros((x_points, y_points, 2))
-    velocity_profile = velocity_profile.at[:,:,0].set(inlet_velocity)
+    velocity_profile = velocity_profile.at[:, :, 0].set(inlet_velocity)
     return velocity_profile
 
-def lbm_update(discrete_velocity_prev: jnp.array, 
-               velocity_profile: jnp.array, 
-               kinematic_viscocity: float,
-               obstacle_mask: jnp.array[jnp.int32]) -> jnp.array:
+
+@jax.jit
+def lbm_update(
+    discrete_velocity_prev: jnp.array,
+    velocity_profile: jnp.array,
+    kinematic_viscocity: float,
+    obstacle_mask,
+) -> jnp.array:
     """
-    The Algorithm
+    The Algorithm at every step.
     """
     # 1. Perscribe outflow BC at right boundary
-    discrete_velocity_prev = discrete_velocity_prev.at[-1,:, LEFT_VELOCITES].set(discrete_velocity_prev[-2,:, LEFT_VELOCITES])
+    discrete_velocity_prev = discrete_velocity_prev.at[
+        -1,
+        :,
+        NodeVelocity.LEFT,
+    ].set(discrete_velocity_prev[-2, :, NodeVelocity.LEFT])
 
     # 2. Compute macroscopic velocities
     density_prev = get_density(discrete_velocity_prev)
-    macroscopic_velocities_prev = get_macroscopic_velocities(discrete_velocity_prev, density_prev)
+    macroscopic_velocities_prev = get_macroscopic_velocities(
+        discrete_velocity_prev, density_prev
+    )
 
     # 3. Inflow Dirichlet with Zou/He scheme
-    macroscopic_velocities_prev = macroscopic_velocities_prev.at[0,1:-1, :].set(
-        velocity_profile[0, 1:-1, :]
-    )
-    density_prev = density_prev[0, :].set(
+    macroscopic_velocities_prev = macroscopic_velocities_prev.at[
+        0, 1:-1, :
+    ].set(velocity_profile[0, 1:-1, :])
+
+    density_prev = density_prev.at[0, :].set(
         (
-            get_density(discrete_velocity_prev[0, :, PURE_VERT_VELOCItieS]. T)
-            +
-            2 * 
-            get_density(discrete_velocity_prev[0, :, LEFT_VEL].T)
-        ) / (
-            1 - macroscopic_velocities_prev[0, :, 0]
+            get_density(
+                discrete_velocity_prev[0, :, NodeVelocity.PURE_VERTICAL].T
+            )
+            + 2
+            * get_density(discrete_velocity_prev[0, :, NodeVelocity.LEFT].T)
         )
+        / (1 - macroscopic_velocities_prev[0, :, 0])
     )
 
     # 4. Compute discrete Equilibria velocites
-    equilibrium_discrete_velocites = get_equilibrium_discrete_velocites(
-        macroscopic_velocities_prev, 
-        density_prev
+    equilibrium_discrete_velocites = get_equilibrium_discrete_velocities(
+        macroscopic_velocities_prev, density_prev
     )
-    discrete_velocity_prev = discrete_velocity_prev.at[0, :, RIGHT_VEL].set(
-        equilibrium_discrete_velocites.at[0, :, RIGHT_VEL]
-    )
+    discrete_velocity_prev = discrete_velocity_prev.at[
+        0, :, NodeVelocity.RIGHT
+    ].set(equilibrium_discrete_velocites[0, :, NodeVelocity.RIGHT])
 
     # 5. Collision Step
 
-    discrete_velocit_post_collision = (
+    discrete_velocity_post_collision = (
         discrete_velocity_prev
-        -
-        relaxation_omega(kinematic_viscocity)
-        *
-        (
-            discrete_velocity_prev
-            -
-            equilibrium_discrete_velocites
-        )
+        - relaxation_omega(kinematic_viscocity)
+        * (discrete_velocity_prev - equilibrium_discrete_velocites)
     )
     # Obstacle mask BC
-    for i,_ in enumerate(N_DISCRETE_VELOCITiES):
-        discrete_velocit_post_collision  = discrete_velocit_post_collision.at[obstacle_mask, LATTICE_INDICIES[i]].set(
-            discrete_velocity_prev[obstacle_mask, OPPOSITE_LATTICE[i]]
+    for i in range(9):
+        discrete_velocity_post_collision = discrete_velocity_post_collision.at[
+            obstacle_mask, Lattice.INDICES[i]
+        ].set(
+            discrete_velocity_prev[obstacle_mask, Lattice.OPOSITE_INDECES[i]]
         )
+
+    # (7) Stream alongside lattice velocities
+    discrete_velocities_streamed = discrete_velocity_post_collision
+    for i in range(9):
+        discrete_velocities_streamed = discrete_velocities_streamed.at[
+            :, :, i
+        ].set(
+            jnp.roll(
+                jnp.roll(
+                    discrete_velocity_post_collision[:, :, i],
+                    Lattice.VELOCITIES[0, i],
+                    axis=0,
+                ),
+                Lattice.VELOCITIES[1, i],
+                axis=1,
+            )
+        )
+
+    return discrete_velocities_streamed
+
+
+def run_simulation(
+    iterations: int,
+    velocity_profile,
+    kinematic_viscocity,
+    obstacle_mask,
+    n_x_points,
+    n_y_ponts,
+    plot_output_steps=100,
+    visualize=True,
+    skip_first_index=0,
+) -> list[SimulationOutput]:
+    """
+    The Simulation.
+    """
+    simulation_output = []
+    # inlet vel profile needs to be set. maybe a function that gives curves
+    discrete_velocities_prev = get_equilibrium_discrete_velocities(
+        velocity_profile,
+        jnp.ones((n_x_points, n_y_ponts)),
+    )
+    for iteration_index in tqdm(range(iterations)):
+        discrete_velocities_next = lbm_update(
+            discrete_velocities_prev,
+            velocity_profile,
+            kinematic_viscocity,
+            obstacle_mask,
+        )
+        discrete_velocities_prev = discrete_velocities_next
+        if (
+            iteration_index % plot_output_steps == 0
+            and visualize
+            and iteration_index > skip_first_index
+        ):
+            density = get_density(discrete_velocities_next)
+            macroscopic_velocities = get_macroscopic_velocities(
+                discrete_velocities_next,
+                density,
+            )
+            velocity_mag = euclidean_norm(macroscopic_velocities)
+
+            # calc_curl - du_dx and dv_dy ignored
+            _, d_u__d_y = jnp.gradient(macroscopic_velocities[..., 0])
+            d_v__d_x, _ = jnp.gradient(macroscopic_velocities[..., 1])
+            curl = d_u__d_y - d_v__d_x
+
+            simulation_output.append(
+                SimulationOutput(
+                    iteration_index,
+                    macroscopic_velocities[..., 0],
+                    macroscopic_velocities[..., 1],
+                    velocity_mag,
+                    curl,
+                )
+            )
+    return simulation_output
